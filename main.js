c2116fd2e8fbf96fb43342576ca17a017f5c968c
Audri <audrey.shi108@gmail.com>
2025-07-27 16:28:26 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const card = document.getElementById("card");
+  const audio = document.getElementById("audio");
+
+  card.addEventListener("mouseenter", function() {
+      audio.play();
+  })
+});
+
+
+let candlecount = 0;
+const gift = document.getElementById("gift");
+
+gift.classList.add("hidden");
+
+function hideButton(buttonId) {
+    const button = document.getElementById(buttonId);
+    button.classList.add('hidden');
+
+    candlecount = candlecount + 1;
+
+    if (candlecount == 15) {
+      gift.classList.remove("hidden")
+      gift.classList.add("visible")
+      gift.classList.add("addzindex")
+    } else {
+      gift.classList.add("hidden")
+      gift.classList.remove("visible")
+    }
+}   
+
+//function onclickoutside(buttonId) {
+  //  const button = document.getElementById(buttonId);
+    //const card = document.getElementById()
+
+//}
+
+function stopAnim() {
+  const button = document.getElementById("noanimbutton")
+  const card = document.getElementById("card")
+  const outside = document.getElementById("outside")
+  card.classList.add("noanim")
+  outside.classList.add("noanim")
+
+
+  
+}
+
+function startAnim() {
+  const button = document.getElementById("yesanimbutton")
+  const card = document.getElementById("card")
+  const outside = document.getElementById("outside")
+  card.classList.remove("noanim")
+  outside.classList.remove("noanim")
+  
+
+}
+
+function openPresent() {
+  const gift = document.getElementById("gift");
+  gift.classList.add("hidden");
+
+}
+
